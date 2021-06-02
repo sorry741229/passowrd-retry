f95dbe6a883a61f27a95ac571b27e8e591b7b8d1
MartinC <sorry741229@gmail.com>
2021-06-02 15:53:36 +0800

    change to password = a123456 and add pwd

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,11 @@
+password = 'a123456'
 x = 3
 while x > 0:
-    password = input('請輸入密碼: ')
-    if password == 'a123456':
+    pwd = input('請輸入密碼: ')
+    if pwd == password:
         print('登入成功!')
         break
-    elif password != 'a123456':
+    elif pwd != password:
         x = x - 1
         print('密碼錯誤!,還有', x, '次機會')
  
